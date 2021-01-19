6af6f07ce018b52f079b6c7205e5cf574d29942d
robFraser1111 <robfraserdesign@gmail.com>
2021-01-19 21:31:07 +1100

    Fixed about page

--- src/components/About.js
+++ src/components/About.js
@@ -9,9 +9,11 @@ export default class About extends Component {
     render() {
         return (
             <section className="justify-self-center self-center text-white text-2xl py-10">
-                Joseph Vea is a singer/songwriter with stories to make an impact on the Australian music scene. With a uniquely smooth and deep tone of voice, he explores multiple genres within each of his tracks. Vea is a musician, an educator, and a stay at home dad who seeks to open conversations often kept in the musical too hard box.
-                <br/><br/>
-                Joseph's debut jam 'Half As Much' explores a journey of self acceptance and love. It is a letter written by our inner voice that tells us that we are 'good enough' and that we deserve to be loved 'Half As Much' as we love others. This song embodies a salsa groove, anchored in undeniable Rnb and Neo Soul goodness.
+                Like many people, I have found myself hooked to my phone and various social media platforms. Whether it be for my music or my personal life, I realised that these things began to take hold of most of my 'free time'. 'Find Me' follows my journey of self-awareness and a commitment to finding out who I am outside of my addiction. This song served as a moment of reflection on who I want to be and how I'd like to use my life.
+                <br></br>
+                'Plastic Man' explores identity through the lens of a man feeling detached from his culture. As the son of parents who immigrated from Tonga to New Zealand, the song explores the risks of denying culture and the impact of that new environment on young children who are left to feel like they don't belong anywhere. The song however goes on to find comfort in knowing there is a place in the world for those who are not strongly connected to their heritage, a place in between these two worlds.
+                <br></br>
+                'Walls' highlights the vital role that vulnerability plays in allowing someone to form closer relationships with those they love. The need to open up to those we care about in life's low moments in order to fully embrace, celebrate and share the highs. It is a call to a scarier, yet fuller way of living. We have always celebrated those who have shown bravery in sport or combat. This song is a call for a new bravery, it's about putting more than your body on the line. 
             </section>
         )
     }
