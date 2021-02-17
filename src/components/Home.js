8be35e52188cfb88e517e43dc2d7ef85fc8d4a78
robFraser1111 <robfraserdesign@gmail.com>
2021-02-17 20:25:28 +1100

    Added bandcamp iframe

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,20 +7,19 @@ export default class Home extends Component {
     }
 
     render() {
+
+        const iframeStyles = {
+            boder: 'none',
+            width: '350px',
+            height: '588px'
+        }
+
         return (
             <section className="grid grid-cols-1 lg:grid-cols-2 gap-4 py-10">
 
-                {/*
                 <div className="justify-self-center self-center">
-                    <iframe title="Spotify" src="https://open.spotify.com/embed/artist/7haTfRcmlhxzWtonlmgPVo" width="300" height="80" frameborder="0"></iframe>
+                    <iframe style={iframeStyles} src="https://bandcamp.com/EmbeddedPlayer/album=4199557519/size=large/bgcol=ffffff/linkcol=0687f5/transparent=true/tracklist=true/tracks=4206688961,2589540885,2544887583,977857814/esig=b682a9a312c2ce0ac9de3d6eb91b52f4/" seamless><a href="https://josephvea.bandcamp.com/album/half-as-much-ep">Half As Much EP by Joseph Vea</a></iframe>
                 </div>
-                
-
-                
-                <div className="youtube justify-self-center self-center">
-                    <iframe title="YouTube" width="560" height="315" src="https://www.youtube.com/embed/olWAhoORHo0" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                </div>
-                */}
                 
             </section>
         )
