2c4369859b9b62e66738538a20012d3bcf187e0f
robFraser1111 <robfraserdesign@gmail.com>
2021-02-22 20:23:26 +1100

    Centered iFrame

--- src/components/Home.js
+++ src/components/Home.js
@@ -15,10 +15,10 @@ export default class Home extends Component {
         }
 
         return (
-            <section className="grid grid-cols-1 lg:grid-cols-2 gap-4 py-10">
+            <section className="grid grid-cols-1 gap-4 py-10">
 
                 <div className="justify-self-center self-center">
-                    <iframe style="border: 0; width: 400px; height: 638px;" src="https://bandcamp.com/EmbeddedPlayer/album=4199557519/size=large/bgcol=ffffff/linkcol=0687f5/transparent=true/tracklist=true/tracks=4206688961,2589540885,2544887583,977857814/esig=b682a9a312c2ce0ac9de3d6eb91b52f4/" seamless><a href="https://josephvea.bandcamp.com/album/half-as-much-ep">Half As Much EP by Joseph Vea</a></iframe>
+                    <iframe style={iframeStyles} src="https://bandcamp.com/EmbeddedPlayer/album=4199557519/size=large/bgcol=ffffff/linkcol=0687f5/transparent=true/tracklist=true/tracks=4206688961,2589540885,2544887583,977857814/esig=b682a9a312c2ce0ac9de3d6eb91b52f4/" seamless><a href="https://josephvea.bandcamp.com/album/half-as-much-ep">Half As Much EP by Joseph Vea</a></iframe>
                 </div>
                 
             </section>
